489d9648489aae881a6cbaa90c1b16d860995a21
Simon Smida <simon.smida@stud.fit.vutbr.cz>
2023-12-12 02:29:10 +0100

    refactoring utils.py

--- utils.py
+++ utils.py
@@ -1,21 +1,35 @@
 import numpy as np
 
+def generate_training_data(input_sequence, context_size, repeat_sequence=True):
+    """
+    Generate training data from the input sequence.
 
-# Model parameters
-context_size = 4
-global_training_history = {'loss': [], 'accuracy': []}
-model = None
+    Args:
+    input_sequence (str): A comma-separated string of integers.
+    context_size (int): The number of elements in each input sequence for the LSTM.
+    repeat_sequence (bool): If True, repeats the sequence to enlarge the dataset.
 
-def generate_training_data(input_sequence, context_size):
-    """Generate training data from the input sequence."""
-    data = [int(x) for x in input_sequence.split(',')] 
-    # Enlarge the sequence by repeating it
-    data *= 10
+    Returns:
+    tuple: Two Numpy arrays, X (input sequences) and y (output values).
+    """
+    try:
+        data = [int(x) for x in input_sequence.split(',')]
+    except ValueError:
+        raise ValueError("Input sequence must be a comma-separated list of integers.")
+
+    if len(data) < context_size:
+        raise ValueError(f"Input sequence length ({len(data)}) is less than the context size ({context_size}).")
+
+    if repeat_sequence:
+        # Repeat the sequence to enlarge the dataset. 
+        # Note: This is for demonstration purposes and might not be ideal for real training scenarios.
+        data *= 10
+
     X, y = [], []
     for i in range(len(data) - context_size):
-        X.append(data[i:i+context_size])
-        y.append(data[i+context_size])
+        X.append(data[i:i + context_size])
+        y.append(data[i + context_size])
     X = np.array(X).reshape(-1, context_size, 1)
     y = np.array(y).reshape(-1, 1)
-    
+
     return X, y
